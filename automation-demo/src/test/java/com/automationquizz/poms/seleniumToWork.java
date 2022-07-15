6e8172e29af21dc679147b3a5b77b3e55e5d1565
MauriceKK <maurice.kabeireho@gmail.com>
2022-07-15 13:02:35 -0400

    updates the EnterText in poms

--- automation-demo/src/test/java/com/automationquizz/poms/seleniumToWork.java
+++ automation-demo/src/test/java/com/automationquizz/poms/seleniumToWork.java
@@ -1,5 +1,25 @@
 package com.automationquizz.poms;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class seleniumToWork {
     
+    private WebDriver driver;
+
+    @FindBy (id="input")
+    public WebElement usernameInput;
+
+    @FindBy (id="Button")
+    public WebElement passwordInput;
+
+
+    
+    public seleniumToWork(WebDriver driver){
+        this.driver = driver;
+       
+        PageFactory.initElements(driver,this);
+}
 }
