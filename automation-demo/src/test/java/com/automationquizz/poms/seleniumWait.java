96495da13c24e15b101c97d3059238e5ce3312d1
MauriceKK <maurice.kabeireho@gmail.com>
2022-07-15 12:33:29 -0400

    Updated TestRunner and and the poms

--- automation-demo/src/test/java/com/automationquizz/poms/seleniumWait.java
+++ automation-demo/src/test/java/com/automationquizz/poms/seleniumWait.java
@@ -1,5 +1,25 @@
 package com.automationquizz.poms;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class seleniumWait {
+    private WebDriver driver;
+
+    @FindBy (id="one")
+    public WebElement something;
+
+    @FindBy (id="submit")
+    public WebElement submitButton;
+
+
     
+    public seleniumWait(WebDriver driver){
+        this.driver = driver;
+       
+        PageFactory.initElements(driver,this);
 }
+}
+
--- automation-demo/src/test/java/com/automationquizz/runner/TestRunner.java
+++ automation-demo/src/test/java/com/automationquizz/runner/TestRunner.java
@@ -8,6 +8,11 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.automationquizz.poms.EnterText;
+import com.automationquizz.poms.seleniumToWork;
+import com.automationquizz.poms.seleniumWait;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
@@ -16,5 +21,29 @@ import io.cucumber.junit.CucumberOptions;
 public class TestRunner {
 
     public static Object EnterTextSelenium;
-    
+    public static WebDriver driver;
+    public static EnterText EnterText;
+    public static seleniumWait seleniumWait;
+    public static seleniumToWork seleniumToWork;
+    public static WebDriverWait Wait;
+
+    @BeforeClass
+    public static void setup(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        EnterText = new EnterText(driver);
+        seleniumWait = new seleniumWait(driver);
+        seleniumToWork = new seleniumToWork(driver);
+        Wait = new WebDriverWait(driver, 2);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+     
+    @AfterClass
+    public static void teardown(){
+        driver.quit();
+        
+    }
 }
+
+    
+
