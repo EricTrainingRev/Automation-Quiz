b34bec76cfe3a7229c2ca7972bb18d16fb6bcd63
quyen513 <quyen513@revature.net>
2022-07-15 11:35:22 -0400

    Quyen

--- quiz/src/test/java/com/revature/steps/PageThreeSteps.java
+++ quiz/src/test/java/com/revature/steps/PageThreeSteps.java
@@ -1,5 +1,8 @@
 package com.revature.steps;
 
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import com.revature.runner.TestRunner;
 
 import io.cucumber.java.en.Given;
@@ -13,17 +16,19 @@ public class PageThreeSteps {
     }
     @When("Selenium enters {int} characters into the input while working around the alerts")
     public void selenium_enters_characters_into_the_input_while_working_around_the_alerts(Integer int1) {
-    
-        TestRunner.pageThree.input.sendKeys("send");
+        for(int i = 0; i<100;i++){
+            TestRunner.pageThree.input.sendKeys("a");
+            TestRunner.driver.switchTo().alert().accept();
+        }
     }
     @When("Selenium clicks the submit button for page three")
     public void selenium_clicks_the_submit_button_for_page_three() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       TestRunner.pageThree.button.click();
     }
     @Then("An alert should pop up with a success message for page three")
     public void an_alert_should_pop_up_with_a_success_message_for_page_three() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.wait.until(ExpectedConditions.alertIsPresent());
+        Assert.assertEquals("SUCCESS!!!", TestRunner.driver.switchTo().alert().getText() );
+        TestRunner.driver.switchTo().alert().accept();
     }
 }
